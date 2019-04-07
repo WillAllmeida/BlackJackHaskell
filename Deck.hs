03355468b914847dd3d0729dd545b3fe004ff47f
William Almeida <will.allmeida@gmail.com>
2019-04-07 12:20:46 -0300

    Adicionando embaralhamento e valores de cartas

--- Deck.hs
+++ Deck.hs
@@ -1,34 +1,69 @@
 module Deck where
 
-import Data.List
 import System.Random
 import Data.Array.IO
 import Control.Monad
-import Data.Map
 
-data Carta = Carta {valor :: Valor, naipe :: Naipe} deriving (Eq)
 
-instance Show Carta where
-  show (Carta v n) = show v ++ " de " ++ show n ++ "s"
+data Card = Card {value :: Value, suit :: Suit} deriving (Eq)
 
-data Naipe = Espada | Ouro | Copa | Pau deriving (Show, Enum, Eq, Ord, Bounded)
+instance Show Card where
+  show (Card v n) = show v ++ " de " ++ show n ++ "s"
 
-todosNaipes :: [Naipe]
-todosNaipes = [ (minBound :: Naipe) .. ]
+data Suit = Espada | Ouro | Copa | Pau deriving (Show, Enum, Eq, Ord, Bounded)
 
-data Valor = A | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Valete | Dama | Rei
+allSuits :: [Suit]
+allSuits = [ (minBound :: Suit) .. ]
+
+type Hand = [Card]
+type Deck = [Card]
+
+data Value = A | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Valete | Dama | Rei
          deriving (Enum, Eq, Ord, Bounded)
 
-instance Show Valor where
+instance Show Value where
   show A = "As"
   show Valete = "J"
   show Dama = "Q"
   show Rei = "K"
   show n = show $ 1 + fromEnum n
 
-todosValores :: [Valor]
-todosValores = [ (minBound :: Valor) .. ]
+allValues :: [Value]
+allValues = [ (minBound :: Value) .. ]
 
 
-baralho :: [Carta]
-baralho = Carta <$> todosValores <*> todosNaipes
+fullDeck :: Deck
+fullDeck = Card <$> allValues <*> allSuits
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+    ar <- newArray len xs
+    forM [1..len] $ \i -> do
+      j <- randomRIO (i,len)
+      vi <- readArray ar i
+      vj <- readArray ar j
+      writeArray ar j vi
+      return vj
+  where
+    len = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray len = newListArray (1, len)
+
+shuffleDeck  :: IO Deck
+shuffleDeck = shuffle fullDeck
+
+cardValues :: Value -> [Int]
+cardValues A   = [1, 11]
+cardValues Two   = [2]
+cardValues Three = [3]
+cardValues Four  = [4]
+cardValues Five  = [5]
+cardValues Six   = [6]
+cardValues Seven = [7]
+cardValues Eight = [8]
+cardValues Nine  = [9]
+cardValues _     = [10]
+
+
+dealCards :: Int -> Deck -> (Hand, Deck)
+dealCards number deck = (take number deck, drop number deck)
