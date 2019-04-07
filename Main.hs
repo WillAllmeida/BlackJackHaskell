7d037c53cf53cc3f91975d056ab7cc3439dc33ef
William Almeida <will.allmeida@gmail.com>
2019-04-07 19:07:48 -0300

    fix sum of score

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Deck
-import Blackjack
 import Data.Char
 import System.Random
 
@@ -11,6 +10,7 @@ data Interface = Interface
   , iFullDeck :: Deck
   , iDraw     :: Deck -> (Hand, Deck)
   , iShuffle  :: IO Deck
+  , iGameOver :: Card -> Bool
   }
 
 
@@ -32,7 +32,12 @@ gameLoop i deck hand = do
     if null yn || not (map toLower yn == "n") then do
       let (hand', deck') = iDraw i deck
       let handP = addHand hand hand'
+
+      let scoreP = sum (map totalValue handP)
+
+      --let scoreC = scoreC + scoreP
       putStrLn ("Your current hand: " ++ show (handP))
+      putStrLn ("Your current score: " ++ show (scoreP))
       gameLoop i deck' handP
 
      else
@@ -40,7 +45,7 @@ gameLoop i deck hand = do
 
 finish :: Interface -> Hand -> Deck -> IO ()
 finish i hand deck = do
-  putStrLn ("Winner: ")
+  putStrLn ("Game Over")
 
 
 
@@ -64,16 +69,35 @@ valueRank Seven = 7
 valueRank Eight = 8
 valueRank Nine  = 9
 valueRank _     = 10
+gameOver card = totalValue card > 21
+
+totalValue :: Card -> Integer
+totalValue (Card value _) = valueRank value
+
+
 
 empty :: Hand
 empty = []
 
+gameOver :: Card -> Bool
+
+--valueC :: Hand -> Integer
+--valueC empty                    = 0
+--valueC hand
+--  | totalValue hand <= 21 = totalValue hand
+--  | totalValue hand > 21  = totalValue hand
+
+--totalValue :: Hand -> Integer
+--totalValue empty = 0
+--totalValue hand = valueRank value + totalValue hand
+-- Values each rank, returns the value in numbers
 
 implementation = Interface
   {  iEmpty     = empty
   ,  iFullDeck  = fullDeck
   ,  iDraw      = dealCards
   ,  iShuffle   = shuffleDeck
+  ,  iGameOver = gameOver
   }
 
 
